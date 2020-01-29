12bd0775dc5a0799d7c6d7855043d2788e9bb81a
Tarmstrong95 <triston95strong@gmail.com>
2020-01-29 14:08:49 -0600

    Updates

---
+++ precache-manifest.afde8a7a0bb8022e9bf9a9a69f86d282.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b96565409681871bebc0d26e7ecba9",
+    "url": "/zipapp/index.html"
+  },
+  {
+    "revision": "9494b9d6135290a85720",
+    "url": "/zipapp/static/css/main.c278b9c1.chunk.css"
+  },
+  {
+    "revision": "0a26e2a3180ace4a5970",
+    "url": "/zipapp/static/js/2.21fd422a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/zipapp/static/js/2.21fd422a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9494b9d6135290a85720",
+    "url": "/zipapp/static/js/main.57008abb.chunk.js"
+  },
+  {
+    "revision": "46770fefe60695df6181",
+    "url": "/zipapp/static/js/runtime-main.7bdda099.js"
+  }
+]);
